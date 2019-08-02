1ae5fc62f466500eae3744254111830b5fe0f773
unknown <liutongtong@focuschina.com>
2019-08-02 15:32:23 +0800

    home page

--- src/pages/header/header1.jsx
+++ src/pages/header/header1.jsx
@@ -1,9 +1,13 @@
 import React, {useState} from 'react';
-import { faStore,faUserCircle,faShoppingCart,faMobileAlt } from "@fortawesome/free-solid-svg-icons";
+import { faStore,faUserCircle,faShoppingCart,faMobileAlt,faChevronDown, faChevronRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Header1(){
-    const [headerdata,setHeaderData] = useState({});
+    const [showLevel2,setShowLevel2] = useState(false);
+    const [showLevel3,setShowLevel3] = useState(false);
+    const [level1ItemId,setLevel1ItemId] = useState(null);
+    const [level2ItemId,setLevel2ItemId] = useState(null);
+    const [level3ItemId,setLevel3ItemId] = useState(null);
     return (
         <div className='header'>
             <div className='header-main'>
@@ -23,6 +27,43 @@ export default function Header1(){
                     </div>
                     <div className='header-app'>
                         <FontAwesomeIcon icon={faMobileAlt}></FontAwesomeIcon>
+                    </div>
+                </div>
+            </div>
+            <div className='header-cat'>
+                <div className='header-cat-item' onMouseLeave={(e)=>{
+                }}>
+                    <div className='header-cat-title'>Category1 <FontAwesomeIcon className='icon-down' icon={faChevronDown}></FontAwesomeIcon></div>
+                    <div className='header-cat-list'>
+                        <div className='header-cat-level header-cat-level1' onMouseEnter={(e)=> {}}>
+                            {
+                                [1,2,3,4,5,6,7,8,9,10].map(i => {
+                                    return (
+                                        <div key={i} className={'header-cat-list-item'}>
+                                            <div className='title'>level1 item {i}<FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon></div>
+                                            <div className='header-cat-level header-cat-level2'>
+                                                {[1,2,3,4,5,6,7,8,9,10].map(j => {
+                                                    return (
+                                                        <div key={j} className='header-cat-list-item'>
+                                                            <div className='title'>level2 item {i*j}<FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon></div>
+                                                            <div className='header-cat-level header-cat-level3'>
+                                                                {[1,2,3,4,5,6,7,8,9,10].map(k => {
+                                                                    return (
+                                                                        <div key={k} className='header-cat-list-item'>
+                                                                            <div className='title'>level3 item {i*j*k}</div>
+                                                                        </div>
+                                                                    );
+                                                                })}
+                                                            </div>
+                                                        </div>
+                                                    );
+                                                })}
+                                            </div>
+                                        </div>
+                                    );
+                                })
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -47,18 +47,53 @@ export default function Home() {
                 </div>
                 <div className='main-prod'>
                     {
-                        [1,2,3,4,5,6].map(item =>{
+                        [1,2,3,4,5,6,7,8,9,10,11,12].map(item =>{
                             return (
                             <div key={item} className='main-prod-item'>
                                 <div className='img-wrap'>
                                     <img src='https://via.placeholder.com/300x300'></img>
                                 </div>
                                 <div className='name'>item</div>
-                                <div className='desc'>This is an item</div>
+                                <div className='desc'>This is an item This is an item This is an item</div>
                             </div>
                             );
                         })
                     }
+                </div>
+                <div className='main-industry'>
+                    <a className='main-industry-image max-1024-hide'>
+                        <img src='https://via.placeholder.com/300x380'></img>
+                        <div className='main-industry-info'>
+                            <div className='main-industry-title'>Industry</div>
+                            <button className='btn btn-main'>Click me</button>
+                        </div>
+                    </a>
+                    <div className='main-industry-prod'>
+                        {
+                            [1,2,3,4,5,6,7,8].map(item =>{
+                                return (
+                                    <div key={item} className='main-industry-item'>
+                                        <img src='https://via.placeholder.com/300x300'></img>
+                                    </div>
+                                );
+                            })
+                        }
+                    </div>
+                </div>
+                <div className='main-form'>
+                    <div className='main-form-bg'>
+                    </div>
+                    <div className='main-form-info max-768-hide'>
+                        <div className='title'>This Is Title</div>
+                        <div className='desc'>This is desc This is desc This is desc This is desc This is desc This is desc </div>
+                    </div>
+                    <div className='main-form-wrap'>
+                        <div className='title'>This Is Title</div>
+                        <input className='input' placeholder='Please enter your name'></input>
+                        <input className='input' placeholder='Please enter your phone'></input>
+                        <input className='input' placeholder='Please enter your advice'></input>
+                        <button className='btn btn-main'>Submit</button>
+                    </div>
                 </div>
             </div>
             <Footer1></Footer1>
